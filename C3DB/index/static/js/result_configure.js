6fcdc253ee8c2c1956befac295785ea5cbb7a6ea
MORPHIUS\JDW <dawoonjung1026@gmail.com>
2017-11-07 20:26:00 +0900

    wdwdwd

--- C3DB/index/static/js/result_configure.js
+++ C3DB/index/static/js/result_configure.js
@@ -92,6 +92,75 @@ function callingConfiguration(setting) {
         $('.result_tpsa').hide();
     }
 
+    if (setting['Heat'] == 1){
+        $('.des_heat').prop('checked', true);
+    } else {
+        $('.des_heat').prop('checked', false);
+        $('.result_heat').hide();
+    }
+
+    if (setting['Total_Energy'] == 1){
+        $('.des_tt_energy').prop('checked', true);
+    } else {
+        $('.des_tt_energy').prop('checked', false);
+        $('.result_tt_energy').hide();
+    }
+
+    if (setting['Elec_Energy'] == 1){
+        $('.des_elc_energy').prop('checked', true);
+    } else {
+        $('.des_elc_energy').prop('checked', false);
+        $('.result_elc_energy').hide();
+    }
+
+    if (setting['Point_Group'] == 1){
+        $('.des_point_group').prop('checked', true);
+    } else {
+        $('.des_point_group').prop('checked', false);
+        $('.result_point_group').hide();
+    }
+
+    if (setting['COSMO_Area'] == 1){
+        $('.des_COSMO_Area').prop('checked', true);
+    } else {
+        $('.des_COSMO_Area').prop('checked', false);
+        $('.result_COSMO_Area').hide();
+    }
+
+    if (setting['COSMO_Volume'] == 1){
+        $('.des_COSMO_Volume').prop('checked', true);
+    } else {
+        $('.des_COSMO_Volume').prop('checked', false);
+        $('.result_COSMO_Volume').hide();
+    }
+
+    if (setting['Ion_Potential'] == 1){
+        $('.des_Ion_Potential').prop('checked', true);
+    } else {
+        $('.des_Ion_Potential').prop('checked', false);
+        $('.result_Ion_Potential').hide();
+    }
+
+    if (setting['HOMO'] == 1){
+        $('.des_HOMO').prop('checked', true);
+    } else {
+        $('.des_HOMO').prop('checked', false);
+        $('.result_HOMO').hide();
+    }
+
+    if (setting['LUMO'] == 1){
+        $('.des_LUMO').prop('checked', true);
+    } else {
+        $('.des_LUMO').prop('checked', false);
+        $('.result_LUMO').hide();
+    }
+
+    if (setting['Dimension'] == 1){
+        $('.des_dimensions').prop('checked', true);
+    } else {
+        $('.des_dimensions').prop('checked', false);
+        $('.result_dimensions').hide();
+    }
 }
 
 
@@ -223,6 +292,106 @@ function userConfigurationChange() {
             $('.result_tpsa').show();
         } else {
             $('.result_tpsa').hide();
+        }
+    });
+
+    $('.des_heat').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_heat').show();
+        } else {
+            $('.result_heat').hide();
+        }
+    });
+
+    $('.des_tt_energy').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_tt_energy').show();
+        } else {
+            $('.result_tt_energy').hide();
+        }
+    });
+
+    $('.des_elc_energy').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_elc_energy').show();
+        } else {
+            $('.result_elc_energy').hide();
+        }
+    });
+
+    $('.des_point_group').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_point_group').show();
+        } else {
+            $('.result_point_group').hide();
+        }
+    });
+
+    $('.des_COSMO_Area').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_COSMO_Area').show();
+        } else {
+            $('.result_COSMO_Area').hide();
+        }
+    });
+
+    $('.des_COSMO_Volume').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_COSMO_Volume').show();
+        } else {
+            $('.result_COSMO_Volume').hide();
+        }
+    });
+
+    $('.des_Ion_Potential').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_Ion_Potential').show();
+        } else {
+            $('.result_Ion_Potential').hide();
+        }
+    });
+
+    $('.des_HOMO').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_HOMO').show();
+        } else {
+            $('.result_HOMO').hide();
+        }
+    });
+
+    $('.des_LUMO').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_LUMO').show();
+        } else {
+            $('.result_LUMO').hide();
+        }
+    });
+
+    $('.des_dimensions').change(function (e) {
+        e.preventDefault();
+        var chk = $(this).is(':checked');
+        if (chk){
+            $('.result_dimensions').show();
+        } else {
+            $('.result_dimensions').hide();
         }
     });
 
